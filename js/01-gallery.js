2af8b552bec611bbe4e5bae0425636167bd032d0
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-01-21 11:33:38 +0200

    ver 0.1
    -added lighbox gallery
    -added basiclighbox gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+function galleryMarkupCreator(array) {
+  return array
+    .map(
+      ({ original, preview, description }) => `
+          <div class="gallery__item">
+    <a class="gallery__link" href=${original}>
+      <img
+        class ="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+      />
+    </a>
+  </div>`
+    )
+    .join("");
+}
+
+galleryRef.innerHTML = galleryMarkupCreator(galleryItems);
+
+galleryRef.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const modal = basicLightbox.create(
+    `
+		<img src=${e.target.dataset.source}>
+	`
+  );
+  modal.show();
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      modal.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+function galleryMarkupCreator(array) {
+  return array
+    .map(
+      ({ original, preview, description }) => `
+    <a class="gallery__item" href=${original}>
+      <img
+        class ="gallery__image"
+        src=${preview}
+        alt=${description}
+      />
+    </a>`
+    )
+    .join("");
+}
+
+galleryRef.innerHTML = galleryMarkupCreator(galleryItems);
+
+galleryRef.addEventListener("click", (e) => {
+  e.preventDefault();
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+});
